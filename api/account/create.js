bc3522712f4c1ecf52cbf81320bd75f02a034f70
Guy Even <gforce2k5@gmail.com>
2019-09-02 10:22:06 +0300

    Made the email unique in accounts collection
    Added a check to find a user with the same email
    in the DB. if the user is found the function stops
    and no user is added while an error message returns
    to the user.

--- api/account/create.js
+++ api/account/create.js
@@ -4,10 +4,12 @@ const Account = require('../../models/account/Account');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-		const {email, name, age} = req.body;
-		const account = new Account({email, name, age});
-		await account.save();
-		return res.send({message: 'success'});
+    const {email, name, age} = req.body;
+    const user = await Account.findOne({email});
+    if (user) return res.send({error: "email already exists"});
+    const account = new Account({email, name, age});
+    await account.save();
+    return res.send({message: 'success'});
 });
 
 module.exports = router;
